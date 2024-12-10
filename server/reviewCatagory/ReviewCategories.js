89f8ff8becde6543d65e1b4bd2c9c030d0d1e08a
AIQUANT13 <samhita.majumder@aiquantgroup.com>
2024-12-10 13:00:50 +0530

    Review catagory update

--- server/reviewCatagory/ReviewCategories.js
+++ server/reviewCatagory/ReviewCategories.js
@@ -2,6 +2,10 @@
 import mongoose, { Schema} from 'mongoose';
 
 const reviewCategorySchema = new Schema({
+  category_id: {
+    type: String,
+    required: true,
+  },
   category_name: {
     type: String,
     required: true,
--- server/reviewCatagory/reviewCategoryRoutes.js
+++ server/reviewCatagory/reviewCategoryRoutes.js
@@ -1,22 +1,38 @@
 import express from 'express';
 import {
-    createReviewCategory, getAllReviewCategories, updateReviewCategory,
+    createReviewCategory, getAllReviewCategories, editReviewCategory,
     deleteReviewCategory,
 } from './reviewCategoryController.js';
 import { verifyToken } from '../auth/jwtUtils.js';
 
 const router = express.Router();
 
+// // Route for creating a new review category
+// router.post('/review-categories',verifyToken, createReviewCategory);
+
+// // Route for getting all review categories
+// router.get('/review-categories',verifyToken, getAllReviewCategories);
+
+// // Update
+// router.put('/review-categories/:id',verifyToken, editReviewCategory);
+
+// // delete
+// router.delete('/review-categories/:id',verifyToken ,deleteReviewCategory);
+
+
+
+
+
 // Route for creating a new review category
-router.post('/review-categories',verifyToken, createReviewCategory);
+router.post('/review-categories', createReviewCategory);
 
 // Route for getting all review categories
-router.get('/review-categories',verifyToken, getAllReviewCategories);
+router.get('/review-categories', getAllReviewCategories);
 
 // Update
-router.put('/review-categories/:id',verifyToken, updateReviewCategory);
+router.put('/review-categories', editReviewCategory);
 
 // delete
-router.delete('/review-categories/:id',verifyToken ,deleteReviewCategory);
+router.delete('/review-categories' ,deleteReviewCategory);
 
 export default router;
