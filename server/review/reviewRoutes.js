1bb73418156d8bf084538ab812ef2fd87fd11af3
AIQUANT13 <samhita.majumder@aiquantgroup.com>
2024-12-13 14:52:41 +0530

    Jwt updated

--- server/review/reviewRoutes.js
+++ server/review/reviewRoutes.js
@@ -1,23 +1,23 @@
 import express from 'express';
 import { createReview, getAllReviews, getReviewsForBusinessUser, getReviewsForEndUser } from './reviewController.js';
-import { verifyToken } from '../auth/jwtUtils.js';
+import { verifyToken, allowBusinessUser, allowEndUser } from '../auth/jwtUtils.js';
 
 const router = express.Router();
 
 // Route to create a review
-router.post('/reviews', createReview);
+router.post('/reviews', verifyToken, allowEndUser, createReview);
 
 // Route to get all reviews
-router.get('/reviews', getAllReviews);
+router.get('/reviews', verifyToken, getAllReviews);
 
 // Get a review by ID
 // router.get('/reviews/:id', getReviewById);
 
 //Get all review - business user
-router.get('/reviews/business', getReviewsForBusinessUser);
+router.get('/reviews/business', verifyToken, allowBusinessUser, getReviewsForBusinessUser);
 
 //Get all review - end user
-router.get('/reviews/user', getReviewsForEndUser);
+router.get('/reviews/user', verifyToken, allowEndUser, getReviewsForEndUser);
 
 
 export default router;
