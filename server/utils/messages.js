d2ff955c2100b250e14f6748cdb4dcc2332e64c1
SRIJIT <srijit.sarkar@aiquantgroup.com>
2024-12-14 17:29:49 +0530

    messages.js file created

---
+++ server/utils/messages.js
@@ -0,0 +1,24 @@
+const messages = {
+    validation: {
+      allFieldsRequired: "All fields are required.",
+      invalidRole: "Invalid role provided.",
+      statusTypeInvalid: "Status must be true (Active) or false (Inactive).",
+      emailExists: "Email is already registered.",
+      emailPasswordRequired: "Email and password are required.",
+      invalidCredentials: "Invalid credentials.",
+      accessDenied: "Access denied."
+    },
+    success: {
+      userCreated: "User successfully created.",
+      loginSuccess: "Login successful."
+    },
+    error: {
+      createUser: "Error creating user.",
+      login: "Error during login.",
+      retrieveUsers: "Error retrieving users."
+    }
+  };
+  
+  export default messages;
+
+  
