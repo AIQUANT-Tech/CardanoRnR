13211f7e2e55d796ad1e65bf83bd5646d5835029
SRIJIT <srijit.sarkar@aiquantgroup.com>
2024-12-16 16:16:07 +0530

    Response heap updated in reviewCategories

--- server/utils/responses.js
+++ server/utils/responses.js
@@ -1,3 +1,4 @@
+
 const responses = {
     validation: {
       allFieldsRequired: "All fields are required.",
@@ -5,22 +6,42 @@ const responses = {
       statusTypeInvalid: "Status must be true (Active) or false (Inactive).",
       emailExists: "Email is already registered.",
       emailPasswordRequired: "Email and password are required.",
-      invalidCredentials: "Invalid credentials.",
+      invalidCredentials: "Invalid credentials fields.",
       accessDenied: "Access denied.",
       accessDeniedBusinessUser: "Access denied. Only Business Users can perform this action.",
       accessDeniedEndUser: "Access denied. Only End Users can perform this",
       Token:"Invalid or expired token",
-      
+      invalid_request_structure: "Invalid request structure.",
+      invalidRequest : "Invalid request",
+      invalidUserName : "Invalid user_name in request header",
+      NoCategories : "No review categories found",
+      categoryListRequired: "Category list is required and must contain at least one category_id",
+      deleteCategory: "No Review Categories found to Delete",
+
+
+      },
+      success: {
+        userCreated: "success",
+      loginSuccess: "success",
+      categoryCreated: "success",
+      success: "success",
+
     },
-    success: {
-      userCreated: "success",
-      loginSuccess: "success"
-    },
+
     error: {
       createUser: "Error creating user.",
       login: "Error during login.",
-      retrieveUsers: "Error retrieving users."
-    }
+      retrieveUsers: "Error retrieving users.",
+      ServerError: "Server error",
+      createCategory: "Error creating review category.",
+      retrieveCategories: "Error retrieving review categories.",
+      updateCategory: "Error updating review category.",
+      deleteCategory: "Error deleting review category.",
+      updateUser: "Error updating user.",
+    },
+
+    
+
   };  
   export default responses;
 
