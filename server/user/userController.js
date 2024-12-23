f708aff0d7b9579dcf43b5ce152c113234b500e4
SRIJIT <srijit.sarkar@aiquantgroup.com>
2024-12-23 09:34:58 +0530

    updated sign up

--- server/user/userController.js
+++ server/user/userController.js
@@ -15,7 +15,8 @@ export const createUser = async (req, res) => {
             return res.status(400).json({user_crud_rs: {status:responses.validation.allFieldsRequired} });
         }
 
-        if (![roles.endUser, roles.endUser].includes(role)) {
+        if (![roles.endUser, roles.endUser].includes(role) || ![roles.businessUser, roles.businessUser].includes(role)
+        ) {
             return res.status(400).json({ user_crud_rs: {status:responses.validation.invalidRole} });
         }
 
