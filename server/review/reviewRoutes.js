c216dd31e60348e4d58688e5a80d2064347c92ba
AIQUANT13 <samhita.majumder@aiquantgroup.com>
2024-12-24 10:55:57 +0530

    Updated

--- server/review/reviewRoutes.js
+++ server/review/reviewRoutes.js
@@ -14,10 +14,10 @@ router.get('/reviews', getAllReviews);
 // router.get('/reviews/:id', getReviewById);
 
 //Get all review - business user
-router.get('/reviews/business/FetchReviews', verifyToken, allowBusinessUser, getReviewsForBusinessUser);
+router.post('/reviews/business/FetchReviews', getReviewsForBusinessUser);
 
 //Get all review - end user
-router.get('/reviews/user/FetchReviews', verifyToken, allowEndUser, getReviewsForEndUser);
+router.post('/reviews/user/FetchReviews', getReviewsForEndUser);
 
 
 export default router;
