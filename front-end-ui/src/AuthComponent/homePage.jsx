f182c568182eb2de995cc1b48876a29300b00692
SRIJIT <srijit.sarkar@aiquantgroup.com>
2025-01-07 09:58:20 +0530

    updated

--- front-end-ui/src/AuthComponent/homePage.jsx
+++ front-end-ui/src/AuthComponent/homePage.jsx
@@ -13,20 +13,19 @@ const HomePage  = () => {
   return (
     <>
       {!showSignUp ? (
-        // This is the home page view
         <div className="home-page-container">
           <div className="left-part">
             <img src={homePageImage} alt="Home" />
           </div>
           <div className="right-part">
             <div className="signin">
-                <h3>Sign In As</h3>
+                <h3>Sign Up As</h3>
             </div>
           <button type="button" className="btn" onClick={handleButtonClick}>
             Business user
           </button>
           <div className="sign-up-link">
-            <h3>All ready have an account, <a href="/login">Log In</a></h3>
+            <h3>Do You all-ready have an account? <a href="/login">Sign In</a></h3>
           </div>
           </div>
         </div>
