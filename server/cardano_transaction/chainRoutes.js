38c7d1c40c777120daf0982c7dc124b825b0bdcc
AIQUANT13 <samhita.majumder@aiquantgroup.com>
2025-01-23 11:11:25 +0530

    Updated

--- server/cardano_transaction/chainRoutes.js
+++ server/cardano_transaction/chainRoutes.js
@@ -1,13 +1,20 @@
 import express from "express";
-import { createTransaction, getTransactionMetadata } from "./index.js";
+import { createTransaction, fetchRedeemers, getTransactionMetadata } from "./index.js";
 
 const router = express.Router();
 
-// Route to create a user
+// Route to create a transaction
 router.post("/createTransaction", createTransaction);
 
 // Route to get metadata
 router.post("/getTransactionMetadata",getTransactionMetadata);
+
+// Route to get redeemer
+router.post("/fetchRedeemers",fetchRedeemers);
+
+
+// Route to get redeemer
+// router.post("/demo",demo);
 
 
 export default router;
