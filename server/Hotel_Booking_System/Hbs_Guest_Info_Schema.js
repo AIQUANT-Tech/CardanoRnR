6d0f0f6ca008efe030d4b76d4b23019d835e1d5d
SRIJIT <srijit.sarkar@aiquantgroup.com>
2025-02-12 16:31:59 +0530

    HBS Integration Updated

---
+++ server/Hotel_Booking_System/Hbs_Guest_Info_Schema.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const GuestInfoSchema = new mongoose.Schema({
+  guest_id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address']
+  },
+  phone_number: {
+    type: String,
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const GuestInfo =new mongoose.model('GuestInfo', GuestInfoSchema);
+
+export default GuestInfo;
