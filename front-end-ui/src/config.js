3ae953a29a16551d966349e949190802be9f317c
AIQUANT13 <samhita.majumder@aiquantgroup.com>
2025-03-05 16:07:56 +0530

    Updated

--- front-end-ui/src/config.js
+++ front-end-ui/src/config.js
@@ -1,4 +1,3 @@
-const API_BASE_URL = "http://13.60.196.125:8080/";
-
-// const API_BASE_URL = "http://localhost:8080/";
+const API_BASE_URL = "http://51.21.61.199:8080/";
+//const API_BASE_URL = "http://localhost:8080/";
 export default API_BASE_URL;
