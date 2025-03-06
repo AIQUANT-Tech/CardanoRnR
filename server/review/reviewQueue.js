fc1f3adc472ebca3e20d00386913e202f90b1ac3
aiquant5 <souvagya.das@aiquantgroup.com>
2025-03-06 16:51:04 +0530

    UI Update

--- server/review/reviewQueue.js
+++ server/review/reviewQueue.js
@@ -1,22 +1,37 @@
 import Queue from "bull";
-import { redeemFunds, scriptAddress } from "../cardano_transaction/cardanoLucid.js";
+import {
+  redeemFunds,
+  scriptAddress,
+} from "../cardano_transaction/cardanoLucid.js";
 import { waitForUTxOWithTimeout } from "./reviewController.js";
 import { Data } from "lucid-cardano";
 import Review from "./Reviews.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-// Create a Bull queue named "reviewQueue" with Redis connection options.
 const reviewQueue = new Queue("reviewQueue", {
-  redis: { host: "127.0.0.1", port: 6379, maxRetriesPerRequest: null },
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    maxRetriesPerRequest:
+      process.env.REDIS_MAX_RETRIES_PER_REQUEST === "null"
+        ? null
+        : Number(process.env.REDIS_MAX_RETRIES_PER_REQUEST),
+  },
 });
 
-// Process jobs added to the queue.
 reviewQueue.process(async (job, done) => {
-  const { reviewId, serializedReviewDatum, serializedReviewRedeemer, lockTxHash } = job.data;
+  const {
+    reviewId,
+    serializedReviewDatum,
+    serializedReviewRedeemer,
+    lockTxHash,
+  } = job.data;
   try {
     // Convert the serialized hex strings back to Constr objects.
     const reviewDatum = Data.from(serializedReviewDatum);
     const reviewRedeemer = Data.from(serializedReviewRedeemer);
-    
+
     console.log("Background job: Waiting for UTxO with txHash:", lockTxHash);
     await waitForUTxOWithTimeout(
       scriptAddress,
@@ -25,7 +40,9 @@ reviewQueue.process(async (job, done) => {
       600000,
       10000
     );
-    console.log("Background job: UTxO found. Redeeming review data on-chain...");
+    console.log(
+      "Background job: UTxO found. Redeeming review data on-chain..."
+    );
 
     const { txHash: redeemTxHash, reputationScore } = await redeemFunds(
       reviewDatum,
@@ -51,5 +68,6 @@ reviewQueue.process(async (job, done) => {
     done(new Error(error));
   }
 });
+console.log("Bull worker is running and waiting for jobs...");
 
 export default reviewQueue;
