bf25b299a6e1dfb868ebd326deb024a2873185aa
AIQUANT13 <samhita.majumder@aiquantgroup.com>
2025-04-08 17:12:22 +0530

    Updated

--- SmartContract/Test/RnRTest.hs
+++ SmartContract/Test/RnRTest.hs
@@ -1,81 +1,244 @@
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE DeriveGeneric     #-}
 
-module Review.RnRTest where
+module Review.QuickCheckTests (tests, mains) where
+
+import Prelude hiding (fail)
+import GHC.Generics (Generic)
+import Control.Monad (replicateM)
+import Data.Maybe (isJust, fromJust)
 
 import Test.Tasty
-import Test.Tasty.HUnit
-import Review.RnR (Review(..), calculateReputation, validateReview, updateReputation)
-import Plutus.V2.Ledger.Contexts (ScriptContext)
-import Prelude (IO, print, putStrLn, Show, FilePath, (<*>), (<$>), return, (.), floor, show, String, undefined, not)
-import Plutus.V2.Ledger.Api ( POSIXTime(..), BuiltinByteString, Validator, ScriptContext, BuiltinData, mkValidatorScript, unsafeFromBuiltinData, unValidatorScript, TxOutRef, Datum(..), txOutDatum, ScriptPurpose(..), TxInfo, txInfoOutputs, TxOut(..), OutputDatum(..), fromData)
-import Plutus.V2.Ledger.Contexts (scriptContextTxInfo)
-import PlutusTx (unstableMakeIsData, compile, toBuiltinData)
-import PlutusTx.Prelude (traceIfFalse, Integer, Maybe(..), Bool(..), ($), (+), (*), (<=), (>), (&&), (/=), (==), (++),traceError, divide, Either(..), any, Eq(..), toBuiltin)
-import qualified Plutus.Script.Utils.V2.Typed.Scripts as Scripts
-import Cardano.Api.Shelley (PlutusScript (PlutusScriptSerialised), writeFileTextEnvelope, displayError, fromPlutusData, toPlutusData)
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.ByteString.Short as SBS
-import Prelude (IO, print, putStrLn, Show, FilePath, (<*>), (<$>), return, (.), floor, show, String)
-import Codec.Serialise (serialise)
-import System.Directory (createDirectoryIfMissing)
-import System.FilePath (takeDirectory)
-import Cardano.Api (PlutusScriptV2, scriptDataToJson, ScriptDataJsonSchema(..), scriptDataFromJson)
-import Prelude (writeFile)
-import System.IO (writeFile, getLine, appendFile)
-import Codec.CBOR.Write (toLazyByteString)
-import PlutusTx (toData, ToData)
-import qualified Data.ByteString.Lazy as LBS
-import Data.Aeson (decode, FromJSON, parseJSON, withObject, (.:), withScientific, withText, encode, Value, eitherDecode)
-import Data.Maybe (fromMaybe)
--- import Codec.Serialise.Class (decode)
-import Control.Monad (mzero)
-import Data.Scientific (toBoundedInteger)
+import Test.Tasty.HUnit (testCase, assertBool, Assertion)
+import Test.Tasty.QuickCheck 
+    ( testProperty
+    , Arbitrary (..)
+    , Positive (..)
+    , Property
+    , chooseInt
+    , elements
+    , (===)
+    , (==>)
+    , Gen
+    , (.&&.)
+    , property
+    , listOf1
+    , vectorOf
+    , suchThat
+    )
+
+-- Plutus imports
+import Plutus.V1.Ledger.Credential (Credential(..))
+import Plutus.V2.Ledger.Api
+import Plutus.V2.Ledger.Contexts
+import qualified PlutusTx
+import qualified PlutusTx.Prelude as P
+import qualified PlutusTx.AssocMap as AssocMap
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
+import PlutusTx.Builtins (toBuiltin)
 
 
--- Mock ScriptContext (Placeholder, as actual ScriptContext requires blockchain state)
-mockCtx :: ScriptContext
-mockCtx = undefined
+import Reputation.RnR
 
--- Test Reputation Calculation
-testCalculateReputation :: TestTree
-testCalculateReputation = testCase "Reputation Calculation" $ do
-    assertEqual "Rep with 50 totalScore, 10 ratings" (calculateReputation 50 10) 2
-    assertEqual "Rep with 1000 totalScore, 50 ratings" (calculateReputation 1000 50) 10
-    assertEqual "Rep with 0 totalScore, 0 ratings" (calculateReputation 0 0) 0
-    assertEqual "Rep with 100 totalScore, 1 rating" (calculateReputation 100 1) 50
 
--- Test Review Validation Logic
-testValidateReview :: TestTree
-testValidateReview = testCase "Validation Logic" $ do
-    let validReview = Review "review123" (Just "prod567") 4 (POSIXTime 1710000000) 100 10 52
-        invalidReviewHighRating = Review "review124" (Just "prod567") 6 (POSIXTime 1710000000) 100 10 52 -- Invalid rating
-        invalidReviewLowRating = Review "review125" (Just "prod567") 0 (POSIXTime 1710000000) 100 10 52  -- Invalid rating
-        validReviewNoRef = Review "review126" Nothing 5 (POSIXTime 1710000000) 200 20 55 -- Valid without reference
-    
-    assertBool "Valid Review Passes" (validateReview validReview mockCtx)
-    assertBool "Valid Review without Reference ID Passes" (validateReview validReviewNoRef mockCtx)
-    assertBool "Invalid Review with Rating > 5 Fails" (not (validateReview invalidReviewHighRating mockCtx))
-    assertBool "Invalid Review with Rating < 1 Fails" (not (validateReview invalidReviewLowRating mockCtx))
+-- Arbitrary Instances for QuickCheck
 
--- Test Review Update Logic
-testUpdateReputation :: TestTree
-testUpdateReputation = testCase "Update Reputation" $ do
-    let review = Review "review789" (Just "prod890") 5 (POSIXTime 1710000000) 50 5 50
-        updatedReview = updateReputation review
-    assertEqual "Updated totalScore" (totalScore updatedReview) 55
-    assertEqual "Updated ratingCount" (ratingCount updatedReview) 6
-    assertBool "Reputation score updated" (reputationScore updatedReview > 0)
+instance Arbitrary PubKeyHash where
+  arbitrary = PubKeyHash . toBuiltin . BS.pack <$> vectorOf 28 (elements ['a'..'z'])
 
--- Main Test Runner
-main :: IO ()
-main = defaultMain $ testGroup "Review Contract Tests"
-    [ testCalculateReputation
-    , testValidateReview
-    , testUpdateReputation
-    ]
+instance Arbitrary BuiltinByteString where
+  arbitrary = toBuiltin . BS.pack <$> listOf1 (elements ['a'..'z'])
+
+instance Arbitrary Review where
+  arbitrary = do
+    ridStr   <- arbitrary `suchThat` (not . null)   
+    refIdStr <- arbitrary `suchThat` (not . null)   
+    rating   <- chooseInt (1, 5)
+    ts       <- (arbitrary :: Gen Integer)          
+    totalS   <- chooseInt (1, 100)
+    count    <- chooseInt (1, 20)
+    pkh      <- arbitrary
+    let rep = calculateReputation (fromIntegral totalS) (fromIntegral count)
+    return $ Review 
+      (toBuiltin $ BS.pack ridStr)
+      (Just $ toBuiltin $ BS.pack refIdStr)
+      (fromIntegral rating)
+      (POSIXTime $ fromIntegral ts)
+      (fromIntegral totalS)
+      (fromIntegral count)
+      rep
+      pkh
+
+instance Arbitrary Redeem where
+  arbitrary = do
+    ridStr <- arbitrary `suchThat` (not . null)
+    pkh    <- arbitrary
+    return $ Redeem (toBuiltin $ BS.pack ridStr) pkh
+
+-- Dummy Builders
+
+dummyTxOutRef :: TxOutRef
+dummyTxOutRef = TxOutRef (TxId "dummyTxId") 0
+
+-- | Build a TxOut carrying the *original* Review datum
+buildOriginalTxOut :: Review -> TxOut
+buildOriginalTxOut review =
+  TxOut
+    (Address (PubKeyCredential $ reviewerPKH review) Nothing)
+    mempty
+    (OutputDatum $ Datum $ PlutusTx.toBuiltinData review)
+    Nothing
+
+-- | Build a TxOut carrying the *updated* Review datum
+buildDummyTxOut :: Review -> TxOut
+buildDummyTxOut review =
+  let updated = updateReputation review
+  in TxOut 
+       (Address (PubKeyCredential $ reviewerPKH review) Nothing)
+       mempty
+       (OutputDatum $ Datum $ PlutusTx.toBuiltinData updated)
+       Nothing
+
+-- | Build a TxInfo carrying the transaction details
+buildDummyTxInfo :: Review -> [TxOut] -> TxInfo
+buildDummyTxInfo review outs = TxInfo
+  { txInfoInputs           = []             
+  , txInfoReferenceInputs  = []
+  , txInfoOutputs          = outs
+  , txInfoFee              = mempty
+  , txInfoMint             = mempty
+  , txInfoDCert            = []
+  , txInfoWdrl             = AssocMap.empty
+  , txInfoValidRange       = always
+  , txInfoSignatories      = [ reviewerPKH review ]
+  , txInfoData             = AssocMap.empty
+  , txInfoId               = TxId "dummyTxId"
+  }
+
+buildScriptContext :: TxInfo -> ScriptContext
+buildScriptContext info = ScriptContext info (Spending dummyTxOutRef)
+
+-- Unit Tests
+
+sampleReview :: Review
+sampleReview = Review
+  { reviewId = "review1"
+  , reviewReferenceId = Just "ref1"
+  , overallRating = 4
+  , timestamp = POSIXTime 1000
+  , totalScore = 10
+  , ratingCount = 2
+  , reputationScore = calculateReputation 10 2
+  , reviewerPKH = PubKeyHash "abc"
+  }
+
+sampleRedeem :: Redeem
+sampleRedeem = Redeem "review1" (reviewerPKH sampleReview)
+
+unitTests :: TestTree
+unitTests = testGroup "Unit Tests"
+  [ testCase "Valid transaction passes" $ do
+      let ctx = buildScriptContext $ buildDummyTxInfo sampleReview [buildDummyTxOut sampleReview]
+      assertBool "Valid tx should pass" $ validateReview sampleReview sampleRedeem ctx
+
+  , testCase "Invalid signer fails" $ do
+      let badRedeem = sampleRedeem { signerPKH = PubKeyHash "wrong" }
+          ctx = buildScriptContext $ buildDummyTxInfo sampleReview [buildDummyTxOut sampleReview]
+      assertBool "Bad signer should fail" $ not $ validateReview sampleReview badRedeem ctx
+
+  , testCase "Invalid rating fails" $ do
+      let badReview = sampleReview { overallRating = 6 }
+          ctx = buildScriptContext $ buildDummyTxInfo badReview [buildDummyTxOut badReview]
+      assertBool "Rating > 5 should fail" $ not $ validateReview badReview sampleRedeem ctx
+
+  , testCase "Empty reference ID fails" $ do
+      let badReview = sampleReview { reviewReferenceId = Just "" }
+          ctx = buildScriptContext $ buildDummyTxInfo badReview [buildDummyTxOut badReview]
+      assertBool "Empty refId should fail" $ not $ validateReview badReview sampleRedeem ctx
+  ]
+
+-- Property Tests
+
+prop_validRating :: Review -> Property
+prop_validRating review =
+  (overallRating review >= 1 && overallRating review <= 5) === True
+
+prop_validReferenceId :: Review -> Property
+prop_validReferenceId review =
+  case reviewReferenceId review of
+    Just ref -> (BS.unpack (fromBuiltin ref) /= "") === True
+    Nothing  -> property False
+
+prop_reputationCalculation :: Review -> Property
+prop_reputationCalculation r =
+  let new = updateReputation r
+      expected = calculateReputation (totalScore r + overallRating r) (ratingCount r + 1)
+  in reputationScore new === expected
+
+prop_signerMatchesAndSigned :: Review -> PubKeyHash -> Property
+prop_signerMatchesAndSigned review pkh =
+  let redeem = Redeem (reviewId review) pkh
+      txInfo = (buildDummyTxInfo review [buildDummyTxOut review])
+                 { txInfoSignatories = [pkh] }
+      ctx = buildScriptContext txInfo
+
+      signerMatches = reviewerPKH review == signerPKH redeem
+      txSignedByReviewer = txSignedBy txInfo (reviewerPKH review)
+
+      expected = signerMatches && txSignedByReviewer
+
+      actual = 
+        let validSigner = reviewerPKH review == signerPKH redeem
+            signed = txSignedBy (scriptContextTxInfo ctx) (reviewerPKH review)
+         in validSigner && signed
+  in actual === expected
+
+prop_validateWithWrongSignerFails :: Review -> Property
+prop_validateWithWrongSignerFails r =
+  let txInfo = buildDummyTxInfo r [buildDummyTxOut r]
+      ctx = buildScriptContext txInfo
+      redeem = Redeem (reviewId r) (PubKeyHash "badSigner")
+  in validateReview r redeem ctx === False
+
+prop_updateIncrementsRatingCount :: Review -> Property
+prop_updateIncrementsRatingCount review =
+  ratingCount (updateReputation review) === (ratingCount review + 1)
+
+prop_updateIncrementsTotalScore :: Review -> Property
+prop_updateIncrementsTotalScore review =
+  totalScore (updateReputation review) === (totalScore review + overallRating review)
+
+prop_identityPreservation :: Review -> Property
+prop_identityPreservation review =
+  let updated = updateReputation review
+  in (reviewId updated === reviewId review)
+     .&&. (reviewerPKH updated === reviewerPKH review)
+
+-- Main Test Tree
+
+tests :: TestTree
+tests = testGroup "Reputation.RnR Tests"
+  [ unitTests
+  , testGroup "QuickCheck Property Tests"
+      [ testProperty "Overall rating is valid" prop_validRating
+      , testProperty "Reference ID is non-empty" prop_validReferenceId
+      , testProperty "Reputation score is correctly updated" prop_reputationCalculation
+      , testProperty "Wrong signer fails" prop_validateWithWrongSignerFails
+      , testProperty "Rating count increments" prop_updateIncrementsRatingCount
+      , testProperty "Total score increments" prop_updateIncrementsTotalScore
+      , testProperty "Review identity is preserved" prop_identityPreservation
+      , testProperty "Signer check matches expectations" prop_signerMatchesAndSigned
+      ]
+  ]
+
+mains :: IO ()
+mains = defaultMain tests
