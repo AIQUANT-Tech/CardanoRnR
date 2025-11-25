c3da28bb46d48c9f3ba1310ee4ab2e6282762e6e
aiquant6 <rajesh.burnwal@aiquantgroup.com>
2025-11-25 10:50:32 +0530

    implemented swagger api

--- server/scheduler/schedulerRoutes.js
+++ server/scheduler/schedulerRoutes.js
@@ -2,6 +2,19 @@ import express from 'express';
 import { processUserMappingFeed } from '../scheduler/schedulerController.js';
 
 const router = express.Router();
+/**
+ * @swagger
+ * /api/scheduler/run:
+ *   post:
+ *     summary: Run user-mapping scheduler manually
+ *     tags: [Scheduler]
+ *     responses:
+ *       200:
+ *         description: Scheduler executed successfully
+ *       500:
+ *         description: Internal server error
+ */
+
 
 router.post("/run", processUserMappingFeed);
 
