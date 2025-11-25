a7aa0c180d82c68a0d1de31bd33ec1fb106b0d1b
aiquant6 <rajesh.burnwal@aiquantgroup.com>
2025-11-25 10:50:32 +0530

    implemented swagger api

---
+++ server/swagger/swagger.js
@@ -0,0 +1,35 @@
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "RnR API",
+      version: "1.0.0",
+      description: "API documentation for RnR backend",
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT}`,
+      },
+    ],
+  },
+
+  apis: [
+    "./reviewCategory/*.js",
+    "./review/*.js",
+    "./user/*.js",
+    "./replyTransactionData/*.js",
+    "./cardano_transaction/*.js",
+    "./Hotel_Booking_System/*.js",
+    "./scheduler/*.js",
+    "./Node_Mailer/*.js",
+  ],
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+
+export function swaggerDocs(app) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
