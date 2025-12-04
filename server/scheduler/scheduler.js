2b9e5b645e55a3e29dac2660881b56dc8e62226d
Arpan <arpan.maitra@aiquantgroup.com>
2025-12-04 16:16:42 +0530

    code updated

--- server/scheduler/scheduler.js
+++ server/scheduler/scheduler.js
@@ -16,6 +16,6 @@ export const initScheduler = () => {
   // Runs every 1 minute (your previous cron was wrong)
   cron.schedule("* * * * *", async () => {
     console.log("Running user mapping scheduler...");
-    await processUserMappingFeed();
+    // await processUserMappingFeed();
   });
 };
